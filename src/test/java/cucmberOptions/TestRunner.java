f97949e842ad2ffcc2e132737cea5f74505d7a92
abondarenko <anastasiia.bondarenko@schange.com>
2021-02-28 13:41:58 +0100

    Attributes

--- src/test/java/cucmberOptions/TestRunner.java
+++ src/test/java/cucmberOptions/TestRunner.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/features",
         glue="stepDefinitions",
+        dryRun = false,
+        monochrome=true,
         plugin={"pretty", "html:target/cucumber.html", "json:target/cucumber.json","junit:target/cucumber.xml"}
         )
 public class TestRunner {
